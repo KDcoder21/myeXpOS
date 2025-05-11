b7d4c7c946f2ea8ab0f9b6c02d3c49d474a91185
Krishna <krishnadeepakvankadari@gmail.com>
2025-05-11 09:07:15 +0530

    Initial commit

---
+++ xfs-interface/stage14.sh
@@ -0,0 +1,9 @@
+load --module 7 $HOME/myexpos/spl/spl_progs/boot_module_14.xsm
+load --int=10 $HOME/myexpos/spl/spl_progs/int_10_s14.xsm
+load --module 5 $HOME/myexpos/spl/spl_progs/module_5_14.xsm
+load --os $HOME/myexpos/spl/spl_progs/os_startup_13.xsm
+load --int=timer $HOME/myexpos/spl/spl_progs/timer_14.xsm
+load --idle $HOME/myexpos/expl/samples/idle_12.xsm
+load --init $HOME/myexpos/expl/samples/assgn_1_11.xsm
+load --exec $HOME/myexpos/expl/samples/even_14.xsm
+load --exec $HOME/myexpos/expl/samples/assgn14.xsm
